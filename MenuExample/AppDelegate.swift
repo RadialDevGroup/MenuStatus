3a3e124e92b0cd6fcf4a8e98ebbf029150b0b017
Geoffrey Thompson <fleaplus@gmail.com>
2018-07-31 11:12:32 -0600

    Initial Commit

---
+++ MenuExample/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenuExample
+//
+//  Created by Geoffrey Thompson on 7/31/18.
+//  Copyright © 2018 Radial Development Group. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
